9f5f08946499938e5e1aa2fb80078b3f05aa41c9
Hugo Juárez <aom921@hotmail.com>
2020-05-29 21:20:06 -0500

    Create Carta.py
    Clase Carta para posteriormente aplicar el deck

---
+++ Carta.py
@@ -0,0 +1,37 @@
+class Carta:
+
+    
+
+
+    """
+    Constructor de un objeto carta
+    numero: Numero de la carta
+    color: Color de la carta
+    efecto: En este argumento se debera preferiblemente con una cadena que describa el efecto especial de alguna carta, como los comodines, toma dos, reversas y saltos.
+    """
+    def __init__(self, numero, color, efecto):
+        self.numero = numero
+        self.color = color
+        if (efecto == ""):  
+            self.efecto = efecto
+        else:
+            self.efecto = "Sin Efecto"
+    
+    """
+    Imprime los atributos de la carta.
+    """
+    def mostrar(self):
+        if(self.efecto == ""):
+             print("{} de color {}".format(self.numero,self.color))
+        else:   
+            print("{} de color {}".format(self.efecto, self.color))
+
+
+"""
+Ejemplo de uso 
+Carta()
+Carta = Carta(1,"Rojo","")
+Carta.mostrar()    
+"""
+
+
