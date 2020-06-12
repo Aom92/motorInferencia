b652e8d342c6ab1082240fae89863af8e35d6f25
Hugo Juárez <aom921@hotmail.com>
2020-06-12 12:47:03 -0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -18,6 +18,7 @@ class UnoGame(arcade.Window):
         #Definir sprites de las cartas.
         self.lista_cartas = arcade.SpriteList()
         self.all_sprites = arcade.SpriteList()
+        self.paused = False
 
     def setup(self):
         """
@@ -28,29 +29,57 @@ class UnoGame(arcade.Window):
 
         #Configurar Mazo
         for num in range(0,108):
-            carta = arcade.Sprite("images/UNO-Card_"+num+".png",SCALING)
+            carta = arcade.Sprite("images/UNO-Card_"+str(num)+".png",SCALING)
             carta.center_y = self.height / 2
-            carta.left = 10
+            #carta.left = 10
             self.all_sprites.append(carta)
 
+    def on_key_press(self, symbol, modifiers):
+        """Manejar los inputs del usuario en el teclado
+        Q: Salir del Juego
+        P: Pausar:
+        Argumentos:
+            symbol {int} -- Cual tecla fue presionada
+            modifiers {int} -- Which modifiers were pressed
+        """
+        if symbol == arcade.key.Q:
+            #Salir inmediatamente
+            arcade.close_window()
+        
+        if symbol == arcade.key.P:
+            self.paused = not self.paused
 
+        if symbol == arcade.MOUSE_BUTTON_RIGHT:
+            lol
+    
+    def on_update(self, delta_time: float):
+        """Actualizar el frame
+        Argumentos:
+            delta_time {float} -- Tiempo desde la ultima actualizacion
+        """
+        #Si estamos pausados, no actualizamos nada
+        if self.paused:
+            return
+
+        #Actualizar todo lo demas
+        self.all_sprites.update()
+
+        #Mas cosas por hacer ...
+
+    def on_draw(self):
+        """Dibuja todos los objetos del juego en pantalla
+        """
+        arcade.start_render()
+        self.all_sprites.draw()
+
+
+
+Juego = UnoGame(SCREEN_HEIGHT,SCREEN_WIDTH,SCREEN_TITLE)
 
 #ABRIR UNA VENTANA:
-arcade.open_window(SCREEN_HEIGHT,SCREEN_HEIGHT,SCREEN_TITLE)
 
-#poner un color de fondo
-arcade.set_background_color(arcade.color.WHITE)
+Juego.setup()
 
-#CLear the screen and start drawing
-arcade.start_render()
-
-#dibujar un circulo
-arcade.draw_circle_filled(
-    SCREEN_WIDTH/2, SCREEN_HEIGHT/2, RADIUS, arcade.color.BLUE
-)
-
-#Terminar el renderizado
-arcade.finish_render()
 
 #Mostrar todo
 arcade.run()
